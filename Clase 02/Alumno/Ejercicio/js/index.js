efbd8a3d93d1ddfeea42992ea3aab7bb606b135c
Santiago Martin <santimartin.lp@gmail.com>
2018-05-31 20:50:32 -0300

    hola

---
+++ Clase 02/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,168 @@
+// EJERCICIO 2
+
+var dia
+
+dia = prompt('Decime un día')
+
+if (dia === 'sabado' || dia === 'domingo') {
+  console.log('Es fin de semana')
+} else {
+  console.log('Es dia de semana')
+}
+
+// 5. Variables
+
+// var fullName
+
+// var fullName1
+
+// var fullName2
+
+// // Privado
+
+// var _fullName
+
+// // Clases
+
+// var FullName
+
+// // No usado
+
+// var $fullName
+
+// var fullName
+
+// console.log('Hola 0 ' + fullName)
+
+// fullName = 'Adrián Ferré'
+
+// console.log('Hola 1 ' + fullName)
+
+// fullName = 'Marcela Correa'
+
+// console.log('Hola 2 ' + fullName)
+
+// fullName = 2
+
+// console.log('Hola 3 ' + fullName)
+
+// var box = 'Soy una caja'
+
+// console.log(box)
+
+// 6. Tipos de valores
+// Boolean
+
+// var value1 = false
+
+// var value2 = true
+
+// // Number
+
+// var number1 = 2
+
+// var number2 = -5
+
+// var pi = 3.1416
+
+// // String
+
+// var fullName = 'Marcela Correa'
+
+// var text = 'jhdskjfhkhjfgkdsjhgfksjgfkjdhskfjhjkjhkjhkj kjhkjhkjhkjhkj jhkjhj'
+
+// var message = 'Hola "Pedro"'
+
+// var message2 = "Hola 'juan'"
+
+// null / undefined
+
+// var undefinedValue = undefined
+
+// var nullValue = null
+
+// Array
+
+// var examsResults = [ 10, 9, 8, 5 ]
+
+// examsResults[3] = 8
+
+// console.log(examsResults[3])
+
+// var students = [ 'Juan', 'Pedro', 'Ramiro' ]
+
+// console.log(students[1])
+
+// var arrayAnyValue = [ 2, 'Ad', false, [ 1, 2, 3 ] ]
+
+// Object
+
+// var age = 29
+
+// var student = {
+//   name: 'Adrián',
+//   lastName: 'Ferré',
+//   age: age
+// }
+
+// var obj = {}
+
+// console.log(student['name'])
+
+// student.name = 'Pepe'
+
+// console.log(student.name)
+
+// 7. Operadores aritméticos
+
+// var value1 = 2
+
+// var value2 = '4'
+
+//  +
+
+// var parsedValue1 = parseFloat(value1, 10)
+
+// var parsedValue2 = parseFloat(value2, 10)
+
+// var resultSum = parsedValue1 + parsedValue2
+
+// console.log(resultSum)
+
+//  -
+
+// var resultRes = value1 - value2
+
+// console.log(resultRes)
+
+//  *
+
+// var resultMul = value1 * value2
+
+// console.log(resultMul)
+
+//  /
+
+// value2 = 0
+
+// var resultDiv = value1 / value2
+
+// console.log(resultDiv)
+
+//  %
+
+// var resultPer = 87687687684 % 2
+
+// console.log(resultPer)
+
+// 8. Web API's y funciones de alto nivel útiles
+// - prompt()
+
+// var value
+
+// value = prompt('Decime un número')
+
+// console.log(value)
+
+// - console.log()
+// - parseInt()
